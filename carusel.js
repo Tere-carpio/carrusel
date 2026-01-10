7dffe90c8e294161d7ce8fc7cbe02f5606ebf966
Tere-carpio <teresa.carpio@alumnos.udit.es>
2026-01-11 00:43:35 +0100

    navbar definitivo

---
+++ carusel.js
@@ -0,0 +1,8 @@
+function showVertical() {
+    const sidebar = document.querySelector('.vertical')
+    sidebar.classList.add('active');
+}
+function hideVertical() {
+    const sidebar = document.querySelector('.vertical')
+    sidebar.classList.remove('active');
+}
